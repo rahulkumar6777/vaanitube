15925ecdb687411444b37bc1e72606ded84708e3
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-24 20:12:51 +0530

    make router

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,7 +1,12 @@
 import { app } from "./index.js";
 
 
+// routes
+import userRoutes from './src/routes/user.routes.js'
 
+
+
+app.use('/api/v1/auth' , userRoutes)
 
 
 app.listen(process.env.PORT, () => {
