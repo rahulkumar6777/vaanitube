4d7c0e64efe7940cb6ad0d705c4d7841c266a760
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-24 22:57:22 +0530

    make model

---
+++ refreshToken-service/src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
+
+const userschema = new mongoose.Schema({
+    userid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["creator", "viewer"]
+    },
+    status: {
+        type: String,
+        enum: ["active", "inactive", "blocked"],
+        required: true,
+    }
+})
+
+
+userschema.methods.generateAccessToken = async function () {
+    return await jwt.sign(
+        {
+            _id: this._id,
+            role: this.role,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userschema.methods.generateRefreshToken = async function () {
+    return await jwt.sign(
+        {
+            _id: this._id,
+            role: this.role
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User = mongoose.model('User' , userschema)
