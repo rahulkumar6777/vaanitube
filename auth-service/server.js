dbd5c5bed08655f1d91139e7d161edbafd974d2d
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-24 20:33:15 +0530

    add

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,4 +1,11 @@
 import { app } from "./index.js";
+import { initRabbitMQ, closeRabbitMQ } from './src/config/rabbitmq.config.js';
+
+
+// Initialize RabbitMQ
+await initRabbitMQ();
+
+
 
 
 // routes
@@ -6,8 +13,19 @@ import userRoutes from './src/routes/user.routes.js'
 
 
 
-app.use('/api/v1/auth' , userRoutes)
+app.use('/api/v1/auth', userRoutes)
 
+
+// Graceful shutdown
+process.on('SIGINT', async () => {
+    await closeRabbitMQ();
+    process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+    await closeRabbitMQ();
+    process.exit(0);
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`);
