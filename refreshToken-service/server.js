400d8aad85d201f261eb4f45d79fe06e7a6f883e
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-25 00:20:10 +0530

    updates

--- refreshToken-service/server.js
+++ refreshToken-service/server.js
@@ -6,7 +6,16 @@ import { initRabbitMQ, closeRabbitMQ } from './src/config/rabbitmq.config.js';
 await initRabbitMQ();
 
 
+// Graceful shutdown
+process.on('SIGINT', async () => {
+    await closeRabbitMQ();
+    process.exit(0);
+});
 
+process.on('SIGTERM', async () => {
+    await closeRabbitMQ();
+    process.exit(0);
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`);
