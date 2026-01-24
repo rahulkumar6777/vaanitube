0e96ff06c5d28100063a34c51fdbc63018e7715c
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-25 00:19:39 +0530

    updates

--- refreshToken-service/server.js
+++ refreshToken-service/server.js
@@ -1,4 +1,11 @@
 import { app } from "./index.js";
+import { initRabbitMQ, closeRabbitMQ } from './src/config/rabbitmq.config.js';
+
+
+// Initialize RabbitMQ
+await initRabbitMQ();
+
+
 
 
 app.listen(process.env.PORT, () => {
