f7c3ec7652998a0fa7933db77801ad0d51cc697c
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-24 23:41:05 +0530

    update

--- refreshToken-service/index.js
+++ refreshToken-service/index.js
@@ -1,6 +1,14 @@
 import express from "express"
 
 
+
+
+// make express app
 const app = express()
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 export { app }
