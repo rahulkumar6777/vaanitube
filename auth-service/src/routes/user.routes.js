a9619297b3836748b706703e50b4a78e8f524825
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-24 20:44:27 +0530

    make routes

--- auth-service/src/routes/user.routes.js
+++ auth-service/src/routes/user.routes.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import { register } from '../controllers/register.controller';
+import { verifyRegister } from '../controllers/verifyregister.controller';
 
 const router = express.Router();
 
 
+
+router.post('/init-register', register)
 
 export default router;
