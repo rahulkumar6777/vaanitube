faebcdce56b9e3fc35e796eb4fb3b10fedac2ce2
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 00:20:22 +0530

    changes made

---
+++ user-service/src/consumers/slices/userCreate.js
@@ -0,0 +1,19 @@
+import { User } from "../../models/user.model.js";
+
+
+export const processUserData = async (data) => {
+    try {
+        const user = new User({
+            userid: data._id,
+            role: data.role,
+            username: data.username,
+            fullname: data.fullname,
+            email: data.email,
+        });
+        await user.save({validateBeforeSave: false});
+
+        console.log('User profile created successfully');
+    } catch (error) {
+        console.error('Error creating user profile:', error);
+    }
+};
