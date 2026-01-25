7d9e20d2462e5baafb6ff62bb317b039369502ba
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 00:11:54 +0530

    changes made

--- user-service/server.js
+++ user-service/server.js
@@ -2,12 +2,23 @@ import { app } from "./index.js";
 import { initRabbitMQ, closeRabbitMQ } from './src/configs/rabbitmq.config.js';
 
 
+const PORT = process.env.PORT;
+
 
 // Initialize RabbitMQ
 await initRabbitMQ();
 
 
-const PORT = process.env.PORT;
+// Graceful shutdown
+process.on('SIGINT', async () => {
+    await closeRabbitMQ();
+    process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+    await closeRabbitMQ();
+    process.exit(0);
+});
 
 app.listen(PORT, () => {
     console.log(` User Service is running on port ${PORT}`);
