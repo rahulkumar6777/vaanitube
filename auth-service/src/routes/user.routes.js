5d6900324efe4585deee1cd3b56c191883195791
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 01:01:24 +0530

    changes made

--- auth-service/src/routes/user.routes.js
+++ auth-service/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { register } from '../controllers/register.controller.js';
 import { verifyRegister } from '../controllers/verifyregister.controller.js';
+import { login } from '../controllers/login.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ const router = express.Router();
 
 router.post('/init-register', register)
 router.post('/verify-register', verifyRegister)
+router.post('/login', login)
 
 export default router;
