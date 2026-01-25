c1c98cb9107522a66aa70d141deb6cbaff880a41
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 01:18:58 +0530

    changes made

--- refreshToken-service/src/controllers/refreshtoken.controller.js
+++ refreshToken-service/src/controllers/refreshtoken.controller.js
@@ -1,6 +1,18 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.model.js';
+
 const refreshToken = async (req, res) => {
     try {
 
+        const RefreshToken = req.cookies.RefreshToken;
+        if (!RefreshToken) {
+            return res.status(401).json({ error: 'No Refresh Token provided' });
+        }
+
+        const decoded = jwt.verify(RefreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const userId = decoded.id;
+
+        
     } catch (error) {
         return res.status(500).json({ error: 'Internal Server Error' });
     }
