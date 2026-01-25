a92212863e71e94e323c6439004c37700883a737
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 00:17:12 +0530

    changes made

--- user-service/src/models/slices/user.model.js
+++ user-service/src/models/slices/user.model.js
@@ -1,7 +1,33 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-
+    userid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    profilepic: {
+        type: String,
+        default: "https://api-devload.cloudcoderhub.in/public/695f91881242b7ee566ffeea/17693668170573e8c5dea066b2d0f26f74a44.jpg"
+    },
+    role: {
+        type: String,
+        required: true
+    },
 })
 
 export const User = mongoose.model('User', userSchema);
