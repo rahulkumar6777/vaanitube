fcf442939a5e6399408b2acca1270d8b0f27c2b9
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 00:11:30 +0530

    changes made

--- user-service/server.js
+++ user-service/server.js
@@ -1,4 +1,10 @@
 import { app } from "./index.js";
+import { initRabbitMQ, closeRabbitMQ } from './src/configs/rabbitmq.config.js';
+
+
+
+// Initialize RabbitMQ
+await initRabbitMQ();
 
 
 const PORT = process.env.PORT;
