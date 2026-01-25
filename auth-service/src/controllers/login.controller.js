6951972aaef800d7b51b8a0ad39f2e94523da6bb
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 00:42:07 +0530

    changes made

--- auth-service/src/controllers/login.controller.js
+++ auth-service/src/controllers/login.controller.js
@@ -1,6 +1,20 @@
+import { body, validationResult } from 'express-validator';
+
+
+const loginValidate = [
+    body('username').isString().notEmpty().withMessage('Username is required'),
+    body('password').isString().notEmpty().withMessage('Password is required'),
+]
+
 const login = async (req, res) => {
     try {
-
+        await Promise.all(loginValidate.map(validation => validation.run(req)));
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        const { username, password } = req.body;
     } catch (error) {
         return res.status(500).json({ message: 'Internal Server Error' });
     }
