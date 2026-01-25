75fddd048f118a98bfe5d036a1b090912a28a0dd
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 00:08:29 +0530

    changes made

--- user-service/index.js
+++ user-service/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 
+
+// dotenv
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+// make express app
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 export { app };
