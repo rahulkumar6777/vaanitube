980f6594d295fa9c27c221e82816dfa59575998c
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-27 00:34:53 +0530

    changes made

---
+++ plan-service/src/controller/plan.controller.js
@@ -0,0 +1,9 @@
+const getPlan = async (req, res) => {
+    try {
+
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export { getPlan };
