e2cf43d3c878dd3598c032db66014e1ca28506f7
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 21:18:14 +0530

    changes made

--- user-service/src/controllers/slices/profile.controller.js
+++ user-service/src/controllers/slices/profile.controller.js
@@ -1,9 +1,11 @@
 import { Model } from '../../models/index.js'
 import { uploadToDevload } from '../../utils/devload.upload.js';
+import fs from 'fs';
+
 
 const ProfileChange = async (req, res) => {
     try {
-        const  userid  = req.user._id;
+        const userid = req.user._id;
 
         const localFilePath = req.file?.path;
 
@@ -14,15 +16,18 @@ const ProfileChange = async (req, res) => {
 
         const updatedUser = await Model.User.findOneAndUpdate(
             { userid },
-            { profilepic: uploadResponse.publicUrl , profilefileid: uploadResponse.fileid },
+            { profilepic: uploadResponse.publicUrl, profilefileid: uploadResponse.fileid },
             { new: true }
         );
+
+        fs.promises.unlink(localFilePath)
 
         return res.status(200).json({
             message: "Profile picture updated successfully",
             profilepic: updatedUser.profilepic,
         });
     } catch (error) {
+        fs.unlinkSync(req.file.path)
         return res.status(500).json({ message: "Internal server error" });
     }
 }
