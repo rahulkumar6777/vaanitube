a7b5eaf8bc5c9e58f2470c8f0a344ea58452e23a
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 13:07:40 +0530

    changes made

--- refreshToken-service/src/controllers/refreshtoken.controller.js
+++ refreshToken-service/src/controllers/refreshtoken.controller.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user.model.js';
+import { AccesstokenOption } from '../utils/option.js';
 
 const refreshToken = async (req, res) => {
     try {
@@ -12,7 +13,18 @@ const refreshToken = async (req, res) => {
         const decoded = jwt.verify(RefreshToken, process.env.REFRESH_TOKEN_SECRET);
         const userId = decoded.id;
 
-        
+        const user = await User.findOne({ userid: userId });
+        if (!user || user.refreshToken !== RefreshToken) {
+            return res.status(403).json({ error: 'Invalid Refresh Token' });
+        }
+
+        const newAccessToken = await user.generateAccessToken();
+
+        return res.status(200)
+            .cookie('AccessToken', newAccessToken, AccesstokenOption)
+            .json({ message: 'Access Token refreshed successfully' });
+
+
     } catch (error) {
         return res.status(500).json({ error: 'Internal Server Error' });
     }
