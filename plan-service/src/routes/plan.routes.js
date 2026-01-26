a93cf31099f50f47cf2b14a64297e787c405a1d0
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-27 00:38:09 +0530

    changes made

--- plan-service/src/routes/plan.routes.js
+++ plan-service/src/routes/plan.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { getPlan } from '../controller/plan.controller.js';
 
 const router = express.Router();
 
-
+router.get('/plan/:id', getPlan)
 
 export default router;
