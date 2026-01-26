ddd3947052d993431dacbf1ca6f7eb7e60664f53
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 20:59:01 +0530

    changes made

--- user-service/src/routes/user.router.js
+++ user-service/src/routes/user.router.js
@@ -1,6 +1,9 @@
 import express from "express";
+import verifyJwt from "../middlewares/verifyJwt.middleware.js";
+import { user } from "../controllers/index.js";
 
 const router = express.Router();
 
+router.post("/profile/change", verifyJwt , user.changeProfilePic)
 
 export default router;
