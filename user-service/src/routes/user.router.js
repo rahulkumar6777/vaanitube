e4fb60760bb3e91bbd76c09d45ea2ad3c9a40f24
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-26 21:17:48 +0530

    changes made

--- user-service/src/routes/user.router.js
+++ user-service/src/routes/user.router.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verifyJwt from "../middlewares/verifyJwt.middleware.js";
 import { user } from "../controllers/index.js";
+import upload from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
-router.put("/profile", verifyJwt , user.changeProfilePic)
+router.put("/profile", verifyJwt , upload.single('file') , user.changeProfilePic)
 
 export default router;
