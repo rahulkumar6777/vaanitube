89685e339ee2bca496a0e1524f8ca7ecba5db0b8
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-28 00:20:53 +0530

    changes made

--- channel-service/index.js
+++ channel-service/index.js
@@ -10,16 +10,20 @@ await dbConnect();
 import { initRabbitMQ } from "./src/configs/rabbitmq.js";
 await initRabbitMQ();
 
+
 const app = exprees();
+
 
 // express middleware
 app.use(exprees.json());
 app.use(exprees.urlencoded({ extended: true }));
+
 
 // seciurity middleware
 import hpp from "hpp";
 import helmet from "helmet"
 app.use(hpp());
 app.use(helmet());
+
 
 export default app;
