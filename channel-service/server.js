5850bbe232619db258142adf53f77b3e908b1c72
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-28 01:49:45 +0530

    changes made

--- channel-service/server.js
+++ channel-service/server.js
@@ -4,7 +4,7 @@ import app from "./index.js";
 const PORT = process.env.PORT || 5003
 
 // rabbitmq connection
-import { initRabbitMQ } from "./src/configs/rabbitmq.js";
+import { closeRabbitMQ, initRabbitMQ } from "./src/configs/rabbitmq.js";
 await initRabbitMQ();
 
 
@@ -14,6 +14,17 @@ import { channelRouter } from "./src/routes/channel.routes.js";
 
 app.use("/api/v1" , channelRouter);
 
+
+// Graceful shutdown
+process.on('SIGINT', async () => {
+    await closeRabbitMQ();
+    process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+    await closeRabbitMQ();
+    process.exit(0);
+});
 
 app.listen(PORT , () => {
     console.log(`Channel service is running on port ${PORT}`);
