57034fedacd53e9cfd0f38afef470e4d7e651c96
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-28 00:53:56 +0530

    changes made

--- channel-service/src/controllers/slices/createChannel.js
+++ channel-service/src/controllers/slices/createChannel.js
@@ -1,6 +1,23 @@
+import { body, validationResult } from 'express-validator';
+
+const channelValidate = [
+    body('name')
+        .notEmpty()
+        .withMessage('Channel name is required')
+        .isLength({ min: 2, max: 100 })
+        .withMessage('Channel name must be between 2 and 100 characters')
+]
+
+
 const createChannel = async (req, res) => {
     try {
-
+        await Promise.all(channelValidate.map(validation => validation.run(req)));
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()[0].msg });
+        }
+        
+        const { name } = req.body;
     } catch (error) {
         console.error('Error creating channel:', error);
         res.status(500).json({ message: 'Internal server error' });
