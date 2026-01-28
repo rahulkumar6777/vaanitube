77eeca137dd0189132bf2442af9db5fb7adc1f1a
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-29 00:47:14 +0530

    changes made

--- channel-subscriber-service/server.js
+++ channel-subscriber-service/server.js
@@ -1,15 +1,5 @@
 import app from "./index.js";
 
-// load environment variable
-import dotenv from 'dotenv'
-dotenv.config()
-
-
-// database connection
-import { dbConnect } from "./src/configs/db.connect.js";
-await dbConnect();
-
-
 
 // server port
 const PORT = process.env.PORT;
