29c1562f01777afcb8bea3f13e0560ca6338f22f
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-28 22:28:16 +0530

    changes made

--- channel-service/src/configs/redis.js
+++ channel-service/src/configs/redis.js
@@ -1,6 +1,6 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+export const client = createClient();
 
 client.on('error', err => console.log('Redis Client Error', err));
 
