ff25619ec7d849b3d39bf699c63635dc403683d2
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-28 22:50:19 +0530

    changes made

--- channel-service/src/routes/channel.routes.js
+++ channel-service/src/routes/channel.routes.js
@@ -1,6 +1,10 @@
 import express from "express";
+import { verifyMiddleware } from "../middlewares/verify.middleware.js";
+import { channel } from "../controllers/index.js";
 
 const channelRouter = express.Router();
+
+channelRouter.get('/channel/validate' , verifyMiddleware , channel.validate)
 
 
 export { channelRouter };
