1399cccad680de7eeebcb6328efa6194793f9d18
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-29 00:03:50 +0530

    changes made

--- channel-service/src/controllers/index.js
+++ channel-service/src/controllers/index.js
@@ -1,7 +1,11 @@
+import { channelAvatar } from "./slices/channelAvatar.js";
+import { channelBanner } from "./slices/channelBanner.js";
 import { channelValidation } from "./slices/ChannelValidation.js";
 import { createChannel } from "./slices/createChannel.js";
 
 export const channel = {
     create: createChannel,
-    validate: channelValidation
+    validate: channelValidation,
+    avatar: channelAvatar,
+    banner: channelBanner
 }
