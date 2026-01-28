a955ef58083f81420a11063c7cee07f4ba152e3e
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-29 00:47:05 +0530

    changes made

--- channel-subscriber-service/index.js
+++ channel-subscriber-service/index.js
@@ -1,5 +1,22 @@
 import express from "express"
 
+
+// load environment variable
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+// database connection
+import { dbConnect } from "./src/configs/db.connect.js";
+await dbConnect();
+
+
+// rabbitmq init
+import { initRabbitMQ } from "./src/configs/rabbitmq.js";
+await initRabbitMQ();
+
+
 const app = express();
+
 
 export default app;
