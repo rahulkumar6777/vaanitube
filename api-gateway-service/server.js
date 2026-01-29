42abf0b2b440055bccaacfe986cdb24b19fd2b87
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-29 17:10:45 +0530

    cors

--- api-gateway-service/server.js
+++ api-gateway-service/server.js
@@ -2,8 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express'
-
 const app = express();
+
+//cors
+import cors from 'cors'
+import { corsOption } from './src/middlewares/cors.middleware';
+app.use(cors(corsOption))
 
 
 app.listen(process.env.PORT, () => {
