dfad6b0a769fb61549947c62d579e8799c118829
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-29 17:30:45 +0530

    add routes

--- api-gateway-service/server.js
+++ api-gateway-service/server.js
@@ -15,11 +15,12 @@ app.use(cors(corsOption))
 
 // routes path
 import authRoutes from './src/routes/auth.routes.js'
-
+import refreshRoutes from './src/routes/auth.routes.js'
 
 
 // routes
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/refresh", refreshRoutes);
 
 
 app.get("/health", (req, res) => res.send("OK"));
