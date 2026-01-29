98716cbcaeff60de50c66bcdb56b07bc00943e6f
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-30 01:49:29 +0530

    changes

--- channel-service/src/routes/channel.routes.js
+++ channel-service/src/routes/channel.routes.js
@@ -1,15 +1,13 @@
 import express from "express";
-import { verifyMiddleware } from "../middlewares/verify.middleware.js";
 import { channel } from "../controllers/index.js";
-import verifyJwt from "../middlewares/verifyJwt.middleware.js";
 
 const channelRouter = express.Router();
 
 
-channelRouter.route('/').post(verifyJwt , channel.create);
-channelRouter.get('/validate'  , channel.validate);
-channelRouter.patch('/:channelId/avatar' , verifyJwt , channel.avatar);
-channelRouter.patch('/:channelId/banner' , verifyJwt , channel.banner);
+channelRouter.route('/').post(channel.create);
+channelRouter.get('/validate', channel.validate);
+channelRouter.patch('/:channelId/avatar', channel.avatar);
+channelRouter.patch('/:channelId/banner', channel.banner);
 
 
 export { channelRouter };
