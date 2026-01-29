153a0cad50135fa4510e67b22c20c1c9cdf7ce84
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-29 17:09:42 +0530

    changes made

---
+++ api-gateway-service/server.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express'
+
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`API Gateway running on port ${process.env.PORT}`);
+});
