9d905ef2c09b7b05ca08732d03e80a41cbca0635
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-30 16:53:11 +0530

    make model

---
+++ channel-subscriber-service/src/models/channelSubscriber.model.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose"
+
+
+const channelSubscriberschema = new mongoose.Schema({
+
+})
+
+export const ChannelSubscriber = mongoose.model("ChannelSubscriber", channelSubscriberschema)
