262b18d6413affe8f9ad431ba63ba25b3a590c50
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-30 16:39:21 +0530

    add middleware on server.js

--- channel-subscriber-service/server.js
+++ channel-subscriber-service/server.js
@@ -5,6 +5,11 @@ import app from "./index.js";
 const PORT = process.env.PORT;
 
 
+// validate internal secret
+import internalSecretMiddleware from "./src/middlewares/internalSecret.middleware.js";
+app.use(internalSecretMiddleware);
+
+
 // server listen
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
