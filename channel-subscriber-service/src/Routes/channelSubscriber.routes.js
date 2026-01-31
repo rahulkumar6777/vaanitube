0b2f5d09f1a953a6b0569e14c7d98fb7ae55411c
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-31 18:10:48 +0530

    make channel un-subscriber route

--- channel-subscriber-service/src/Routes/channelSubscriber.routes.js
+++ channel-subscriber-service/src/Routes/channelSubscriber.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { channelSubscribe } from '../controllers/channelSubscribe.controller.js';
+import { channelUnsubscribe } from '../controllers/channelUnsubscriber.controller.js';
 
 const channelSubscriberRoutes = express.Router();
 
-channelSubscriberRoutes.route('/').post(channelSubscribe)
+channelSubscriberRoutes.route('/').post(channelSubscribe).delete(channelUnsubscribe)
 
 export default channelSubscriberRoutes;
