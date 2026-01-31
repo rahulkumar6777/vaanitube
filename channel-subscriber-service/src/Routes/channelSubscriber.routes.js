8301348f8ebf687fdcc85563bdfb770c4f20f7f3
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-31 18:09:54 +0530

    make channel subscriber route

--- channel-subscriber-service/src/Routes/channelSubscriber.routes.js
+++ channel-subscriber-service/src/Routes/channelSubscriber.routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { channelSubscribe } from '../controllers/channelSubscribe.controller.js';
 
 const channelSubscriberRoutes = express.Router();
 
+channelSubscriberRoutes.route('/').post(channelSubscribe)
 
 export default channelSubscriberRoutes;
