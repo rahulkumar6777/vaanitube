daf829525e1cdd1cac2408deee62eb288f3ab76f
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-01 21:14:17 +0530

    channel model

---
+++ refactor/channel-domain/src/models/slices/channels.model.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+
+const channelschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    channelUsername: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+        lowercase: true,
+        trim: true
+    },
+    channelAvatar: {
+        type: String,
+        required: false,
+    },
+    channelAvatarId: {
+        type: String,
+        required: false,
+    },
+    channelBannerId: {
+        type: String,
+        required: false,
+    },
+    channelBanner: {
+        type: String,
+        required: false,
+    },
+    ownerId: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    totalSubscribers: {
+        type: Number,
+        default: 0,
+    },
+    totalvideos: {
+        type: Number,
+        default: 0,
+    },
+    totalViews: {
+        type: Number,
+        default: 0,
+    },
+    status: {
+        type: String,
+        enum: ['draft', 'active', 'suspended'],
+        required: true,
+        default: 'draft'
+    },
+    planId: {
+        type: String,
+        default: null
+    },
+    activatedAt: {
+        type: Date,
+        default: null
+    }
+})
+
+export const Channel = mongoose.model('Channel', channelschema);
