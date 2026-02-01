40c22987ad3a2192a84376c4b54f22622f4218d8
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-01 21:27:00 +0530

    add routes

---
+++ refactor/channel-domain/src/routes/channel.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { channel } from "../controllers/index.js";
+
+const channelRouter = express.Router();
+
+
+channelRouter.route('/').post(channel.create);
+channelRouter.get('/validate', channel.validate);
+channelRouter.patch('/:channelId/avatar', channel.avatar);
+channelRouter.patch('/:channelId/banner', channel.banner);
+
+
+export { channelRouter };
