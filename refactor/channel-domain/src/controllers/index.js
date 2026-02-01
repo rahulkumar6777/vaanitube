22782550293079fd098ff1f7c8ddfa894ea03051
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-01 23:02:06 +0530

    change

--- refactor/channel-domain/src/controllers/index.js
+++ refactor/channel-domain/src/controllers/index.js
@@ -2,10 +2,16 @@ import { channelAvatar } from "./slices/channelAvatar.js";
 import { channelBanner } from "./slices/channelBanner.js";
 import { channelValidation } from "./slices/ChannelValidation.js";
 import { createChannel } from "./slices/createChannel.js";
+import { channelSubscribe } from "./slices/channelSubscribe.controller.js";
+import { channelUnsubscribe } from "./slices/channelUnsubscribe.controller.js";
+import { getSubscriberCount } from "./slices/channelSubscriber.controller.js";
 
 export const channel = {
     create: createChannel,
     validate: channelValidation,
     avatar: channelAvatar,
-    banner: channelBanner
+    banner: channelBanner,
+    subscribe: channelSubscribe,
+    unsubscriber: channelUnsubscribe,
+    Subscibers: getSubscriberCount
 }
