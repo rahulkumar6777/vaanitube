d6c9c236b6bea075cb7844192a38fc86a3f1f5bd
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-01 23:03:31 +0530

    add new route

--- refactor/channel-domain/src/routes/channel.routes.js
+++ refactor/channel-domain/src/routes/channel.routes.js
@@ -9,6 +9,7 @@ channelRouter.get('/validate', channel.validate);
 channelRouter.patch('/:channelId/avatar', channel.avatar);
 channelRouter.patch('/:channelId/banner', channel.banner);
 channelRouter.post('/:channelId/subscribe' , channel.subscribe)
+channelRouter.post('/:channelId/unsubscribe' , channel.unsubscriber)
 
 
 export { channelRouter };
