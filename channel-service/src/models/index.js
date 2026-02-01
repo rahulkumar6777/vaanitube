88c25d925f6cd4e02bf9f01cfa389b54fffc4d34
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-01 23:51:26 +0530

    remove channel and channel-subscriberfrom main

--- channel-service/src/models/index.js
+++
@@ -1,7 +0,0 @@
-import { Channel } from "./slices/channels.model.js";
-import { ChannelSetting } from "./slices/channelsettings.model.js";
-
-export const Model = {
-    Channel,
-    ChannelSetting
-}
--- channel-subscriber-service/src/configs/redis.js
+++
@@ -1,9 +0,0 @@
-import { createClient} from "redis"
-
-export const client = createClient()
-
-client.on('error' , err => console.log("redis connection Error" , err));
-
-export const connectRedis = async () => {
-    await client.connect();
-}
