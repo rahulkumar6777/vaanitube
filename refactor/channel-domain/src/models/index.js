009221621516eff4925dd08402f289494929f7c2
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-01 23:01:53 +0530

    change

--- refactor/channel-domain/src/models/index.js
+++ refactor/channel-domain/src/models/index.js
@@ -2,7 +2,7 @@ import { channel } from "./slices/channels.model.js"
 import { ChannelSetting } from "./slices/channelsettings.model.js"
 import { ChannelSubscriber } from "./slices/channelSubscriber.model.js"
 
-export const channel = {
+export const Model = {
     channel,
     ChannelSetting,
     ChannelSubscriber
