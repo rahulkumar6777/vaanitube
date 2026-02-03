ba508b2968eeda856c72e5ebdf5ad3c2592d26d1
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-03 21:28:28 +0530

    update

--- channel-service/src/models/index.js
+++ channel-service/src/models/index.js
@@ -1,3 +1,4 @@
+import { ChannelMember } from "./slices/channelMember.model.js"
 import { channel } from "./slices/channels.model.js"
 import { ChannelSetting } from "./slices/channelsettings.model.js"
 import { ChannelSubscriber } from "./slices/channelSubscriber.model.js"
@@ -5,5 +6,6 @@ import { ChannelSubscriber } from "./slices/channelSubscriber.model.js"
 export const Model = {
     channel,
     ChannelSetting,
-    ChannelSubscriber
+    ChannelSubscriber,
+    ChannelMember
 }
