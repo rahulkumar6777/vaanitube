c6d5420e873c7d4ec398fe6c9cea4a1278c0cbf4
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-05 01:37:18 +0530

    redis conn

--- worker/transcoding/worker.js
+++ worker/transcoding/worker.js
@@ -3,3 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 
+// redis connection
+import { Redis } from 'ioredis';
+const connection = new Redis({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT),
+    maxRetriesPerRequest: null,
+});
