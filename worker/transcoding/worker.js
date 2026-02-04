016a462266034027a6ce416daa3504837862cc57
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-05 01:40:24 +0530

    paths

--- worker/transcoding/worker.js
+++ worker/transcoding/worker.js
@@ -10,3 +10,18 @@ const connection = new Redis({
     port: parseInt(process.env.REDIS_PORT),
     maxRetriesPerRequest: null,
 });
+
+
+// Paths and filenames
+const ffmpegPath = '/usr/bin/ffmpeg';
+const outputDir = 'hls_output';
+const thumbnailoutputDir = 'thumbnailOutput';
+const uploadsDir = 'uploads';
+const ffprobePath = "/usr/bin/ffprobe";
+const resizedThumbnailPath = 'resizedthumbnail'
+
+
+if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir, { recursive: true });
+if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
+if (!fs.existsSync(thumbnailoutputDir)) fs.mkdirSync(thumbnailoutputDir, { recursive: true });
+if (!fs.existsSync(resizedThumbnailPath)) fs.mkdirSync(resizedThumbnailPath, { recursive: true });
