5ece77bdc600d0996a76eb378cef4863144bba3e
Rahulkumar6777 <rahulk48546@gmail.com>
2026-02-05 21:30:45 +0530

    add coments

--- channel-service/src/consumers/slices/index.js
+++ channel-service/src/consumers/slices/index.js
@@ -5,8 +5,9 @@ import { assertQueue } from "../../configs/rabbitmq.js";
 
 export const consumer = async () => {
 
-    // Step 1: Ensure queue exists
+    // queue exist karta hai ki nhi ye check kar rahe hai 
     await assertQueue("channel.member.join", { durable: true });
 
+    // and yeha pe hum consume kar rahe hai
     await consumeQueue('channel.member.join', joinChannelMemberShip)
 }
