accc95fed6e58f292ecc7f110353b6a608d84b2f
DeployHub Builder <rahulk48546@gmail.com>
2026-07-19 01:01:53 +0530

    feat: add Redis connection setup and integrate with server initialization

--- monolithic/api/src/server.js
+++ monolithic/api/src/server.js
@@ -1,4 +1,5 @@
 import { connectToMongoDB, disconnectFromMongoDB } from "./config/dataBases/mongoDb.js";
+import { connectRedis } from "./config/redis/redis.js";
 import app from "./index.js";
 import { envs } from "./lib/env.js";
 
@@ -15,5 +16,6 @@ process.on("SIGTERM", async () => {
 
 app.listen(envs.PORT, async () => {
     await connectToMongoDB();
+    await connectRedis();
     console.log(`Server is running on port ${envs.PORT} in ${envs.NODE_ENV} mode`);
 });
