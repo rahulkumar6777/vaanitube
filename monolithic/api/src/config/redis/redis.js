2db5e26c030545ab681d4a3f71836c7b483f25b7
DeployHub Builder <rahulk48546@gmail.com>
2026-07-19 22:51:16 +0530

    refactor: streamline Redis connection logic for production and development environments

--- monolithic/api/src/config/redis/redis.js
+++ monolithic/api/src/config/redis/redis.js
@@ -4,16 +4,26 @@ import { envs } from '../../lib/env.js';
 
 let redis;
 
-
-
-
 export const connectRedis = async () => {
-    redis = new Redis(`${envs.REDIS_URL}`, {
-        enableReadyCheck: true,
-        lazyConnect: true
-    });
 
-    await redis.set('foo', 'bar');
+    // here i use external free redis for production
+    if (envs.NODE_ENV === 'production') {
+        redis = new Redis(`${envs.REDIS_URL}`, {
+            enableReadyCheck: true,
+            lazyConnect: true
+        });
+
+        await redis.set('foo', 'bar');
+    } else {
+        redis = new Redis({
+            host: `${envs.NODE_ENV === 'production' ? `${envs.REDIS_HOST}` : 'localhost'}`,
+            port: `${envs.NODE_ENV === 'production' ? `${envs.REDIS_PORT}` : '6379'}`,
+            enableReadyCheck: true,
+            lazyConnect: true
+        });
+
+        await redis.connect();
+    }
     console.log("Redis Connected")
     return redis;
 }
